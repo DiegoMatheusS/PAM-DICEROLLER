bafc8a9a1339514bb543eda8036598279aeea830
dms.diego.2019@gmail.com <dms.diego.2019@gmail.com>
2024-03-25 20:35:53 -0300

    feat: adicionado botao

---
+++ DICEROLLER/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace DICEROLLER
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculaTip(object sender, EventArgs e)
+        {
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Generating 6 random numbers:");
+
+            for (uint ctr = 1; ctr <= 6; ctr++)
+                Console.WriteLine($"{rnd.Next(),15:N0}");
+
+
+        }
+        
+        private void BottonPickerCalcule(object sender, EventArgs e)
+        {
+            int clck = CalculaTip();
+            double amount = double.Parse(Dado.Text);
+        }
+
+    }
+
+}
